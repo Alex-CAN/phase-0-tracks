8d28215ed2f85e779d217d0664a73e39b2b5d674
Alex C. Nelon <caminelson.cn@gmail.com>
2016-08-29 10:21:07 -0700

    4.6

---
+++ ruby/seceret_agent.rb
@@ -0,0 +1,32 @@
+#ask user if they'd like to decrypt or encrypt
+
+#Encryption:
+#ask user for "secret_password"
+#string are zero indexed
+index = 0
+#takes the "secret_password" string and returns a string with each letter changed next letter in the alaphabet.
+def (secret_password)
+	while index < secret_password.length
+  	p secret_password[index].next
+ 	index += 1
+ 	end
+end
+
+#Decryption:
+#create an empty array for secret_password
+secret_password = []
+#ask user to input secret_password one letter at a time and type done when theyre done.
+p "first letter"
+	letter = gets.chomp
+if letter.length > 0
+	until letter.length == 0
+#push letters to empty secret_password array.
+	secret_password << letter
+end
+else
+p "next letter"
+letter = gets.chomp
+end
+
+secret_password.each {|letter|
+}
