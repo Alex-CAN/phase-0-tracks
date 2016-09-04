7f3993fb62dfb7395c83609f53186c57d3b954d4
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-04 13:11:16 -0700

    5.6

--- ruby/arr_practice.rb
+++ ruby/arr_practice.rb
@@ -4,7 +4,9 @@ coffeeshops = ["Starbucks", "Petes", "Philz", "Sojuus", "Coffee Bar"]
     coffeeshops.each {|x| puts "#{x} is the best coffeshop!"}
    p coffeeshops
 
-coffeeshops = coffeeshops.map {|x| x = "N"}
+coffeeshops = coffeeshops.map {|x| 
+	while coffeeshops[x]
+	}
 p coffeeshops
 
 coffeeshops.map! {|x| puts "#{x} is the best coffeshop!"}
