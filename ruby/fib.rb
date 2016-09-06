3736e689df81bcfe64f0d3a37fb6573b26289f06
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-06 07:35:20 -0700

    5.6

--- ruby/fib.rb
+++ ruby/fib.rb
@@ -10,12 +10,18 @@
 # p fib(5)
 
 
-def fib(x)
-	fib_arr = [0,1]
-	index = 0
-	x.times 
-	fib_arr << fib_arr[0] + fib_arr[1]
-	index += 1
+# def fib(x)
+# 	fib_arr = [0,1]
+# 	index = 0
+# 	x.times 
+# 	fib_arr << fib_arr[0] + fib_arr[1]
+# 	index += 1
+# end
+
+# p fib (6)
+
+def try_stuff(x)
+	x.downto(1) { |n| p 1+x}
 end
 
-p fib (6)
+try_stuff(5)
