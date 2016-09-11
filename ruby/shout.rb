c6c31f1b993bd76a122b2a5939caac6d4af75b53
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-10 18:28:19 -0700

    6.4 modules

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -12,23 +12,46 @@
 
 # p Math.singleton_methods
 
+# module Shout
+# 	def self.yell_angrily(words)
+#     words + "!!!" + " :("
+# 	end
+
+# 	def self.yell_happily(words)
+# 		words + "!!!" + ":)"
+# 	end
+
+# 	def self.bipolar
+# 		p "I'm so mad about..."
+# 		p self.yell_angrily("People suck")
+# 		p "I'm so happy about..."
+# 		p self.yell_happily("Animals rule")
+# 	end
+# 	self.bipolar
+# end
+
+
+#  p Shout
+
 module Shout
-	def self.yell_angrily(words)
-    words + "!!!" + " :("
+	def yell_angry(words)
+		words + "!!!"
 	end
 
-	def self.yell_happily(words)
-		words + "!!!" + ":)"
+	def yell_happy(words)
+		words + "!!!"
 	end
-
-	def self.bipolar
-		p "I'm so mad about..."
-		p self.yell_angrily("People suck")
-		p "I'm so happy about..."
-		p self.yell_happily("Animals rule")
-	end
-	self.bipolar
 end
 
+class Crocket
+	include Shout
+end
 
- p Shout
+class JTHM
+	include Shout
+end
+
+crocket = Crocket.new
+p crocket.yell_angry("People Suck")
+jthm = JTHM.new
+p jthm.yell_happy("Suck people")
