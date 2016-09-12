74f0854af3863c2d851339f687132027b6187335
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-12 11:00:52 -0700

    shopping list

--- ruby/gps_2.2.rb
+++ ruby/gps_2.2.rb
@@ -1,10 +1,30 @@
 def add_item(cart, item)
-	cart[item] = []
+	cart[item] = ""
+end
+
+def add_quantity(cart, item, quantity)
+	cart[item] = quantity
+	cart
+end
+
+def remove_item(cart, item)
+	cart.delete(item)
+	cart
 end
 cart = {}
 
 list = ["a" , "b" , "c"]
 
+
 list.each { |item| add_item(cart, item) }
 
 p cart
+
+p add_quantity(cart, "b", 3)
+
+p remove_item(cart, "a")
+#interface
+
+def print_cart(cart)
+	p cart
+end
