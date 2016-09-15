9c9a184a8673508f8c6943b259b448d279bf21f7
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-14 23:15:41 -0700

    week 7

--- ruby/6.6/game.rb
+++ ruby/6.6/game.rb
@@ -1,77 +1,58 @@
 class Guessing
 	attr_reader :guess_count
-	attr_reader :game_on
+
 
 	def initialize(answer)
 		@right_answer = answer
-		@game_on = true
 		@guess_count = answer.length
 		p "Lets play!"
 	end
 
 	def your_word
-		p "-" * @guess_count
+		p "_" * @guess_count
 	end
 
 	def guess_check(guess)
-		@guess_count -=1
-		if @right_answer.include?(guess)
-			@game_on
+		check = []
+		guess.each { |letter| 
+			if !@right_answer.include?(letter)
+				check <<  "x"
+			else
+				check << "o"
+			end
+		}
+		if check.include?("x")
+			puts "SORRY YOU LOSE!"
 		else
-			return false
+			puts "WINNER!"
 		end
-	end
-
-	def final_check(final_answer)
-			p "Your Answer"
-			final_answer.join
-			p "Right Answer"
-			right_answer.join
 	end
 end
 
-
-final =[]
-
+final = []
+win_word = []
 p "Player one input word"
 answer = gets.chomp.split
 
 game = Guessing.new(answer)
 game.your_word
 
-p "Player two make a guess."
+until final.length == answer.length
+p "Player two make a guess"
 guess = gets.chomp
-
-
-case 
-when game.guess_check(guess) == true
-	p "Nice guess"
-	final << guess
-	p "Player two make a guess."
-	guess = gets.chomp
-when game.guess_check(guess) == false
-	p "Bad guess"
-	final << guess
-	p "Player two make a guess."
-	guess = gets.chomp
-when final.length == answer.length
-	game.final_check(final)
+final << guess	
+p "_"* (game.guess_count - final.length)
 end
 
+game.guess_check(final)
 
 
 
 
 
-# while !
-# game.game_on
-# 	p "Player two make a guess"
-# 	guess = gets.chomp
-#   if game.guess_check(guess) == true
-#   	final << guess
-#   	p "Player two make a guess"
-# 	guess = gets.chomp
-#   end
-# end
 
-# puts "You won"
+
+
+
+
+	
---
+++ ruby/6.6/game_spec.rb
@@ -0,0 +1,9 @@
+#RSPEC
+# 'describe' block for each group of text
+# 'it' block for each individual test
+# expect (<CODE>).to eq <RESULT>
+
+describe Guessing it "stores the word and its length" do 
+
+	
+end
