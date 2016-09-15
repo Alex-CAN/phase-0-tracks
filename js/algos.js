c45b3e8ad2badf2b2dbcd1ecd3d011a0bf8e1f6f
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-14 23:07:05 -0700

    7.3:JavaScrpit

---
+++ js/algos.js
@@ -0,0 +1,12 @@
+//function takes on argument; an array
+// for each string in the array get its length
+// compare array of lengths
+// print largest number
+
+ var strings = ["abc", "blah", "ad"];
+
+ strings.sort(function(a, b) {
+ 	return a - b;
+ 	 console.log(strings)
+ });
+
