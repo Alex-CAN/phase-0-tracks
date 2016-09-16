fe7d88b9b3ac00c506075ac57e84e8de22d9be86
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-16 11:26:17 -0700

    accessing array by index

--- ruby/6.6/game.rb
+++ ruby/6.6/game.rb
@@ -5,11 +5,14 @@ class Guessing
 	def initialize(answer)
 		@right_answer = answer
 		@guess_count = answer.length
+		@guesser_word = Array.new(@guess_count, "-")
 		p "Lets play!"
 	end
 
-	def your_word
-		p "_" * @guess_count
+	def your_word(guess)
+		index = @right_answer.index(guess)
+		@guesser_word[index] = guess
+		p @guesser_word
 	end
 
 	def guess_check(guess)
@@ -32,18 +35,21 @@ end
 final = []
 win_word = []
 p "Player one input word"
-answer = gets.chomp.split
+answer = gets.chomp.chars
 
 game = Guessing.new(answer)
-game.your_word
+# game.your_word
+p answer
 
 until final.length == answer.length
 p "Player two make a guess"
 guess = gets.chomp
+puts game.your_word(guess)
+p answer
 final << guess	
-p "_"* (game.guess_count - final.length)
 end
 
+p answer
 game.guess_check(final)
 
 
--- ruby/6.6/game_spec.rb
+++ ruby/6.6/game_spec.rb
@@ -2,8 +2,18 @@
 # 'describe' block for each group of text
 # 'it' block for each individual test
 # expect (<CODE>).to eq <RESULT>
+require_relative 'game'
 
-describe Guessing it "stores the word and its length" do 
+describe Guessing do
+	let(:game) {Guessing.new(["w","o","r","d"])}
+
+ it "encrypts word to all dashes" do 
+ 	expect(game.your_word). to eq "----"
+ end
+
+ # it "gives update on guess status" do
+ # 	expect(game.word_update("r")). to eq "--r-"
+ # end
 
 	
 end
