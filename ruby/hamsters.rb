558957be407b5e60b6aa001a5445a2643daf2849
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-17 14:11:24 -0700

    refractor: classes

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,29 +1,57 @@
+class Hamster
+	attr_reader = :name, :color, :volume, :age
+	def initialize(name, fur_color, volume, age)
+		@name = name
+		@color = fur_color
+		@volume = volume
+		@age = age
+		p "Initialzing Hamster Mode..."
+	end
+	def hamster
+		hello
+		hamster_age
+		hamster_volume
+		fur
+	end
+
+	def hello
+		p "======================"
+		p "Hello #{@name}"
+		p "======================"
+	end
+	def hamster_age
+		p "Wow #{@name} is #{@age} years old!"
+		p "======================"	
+	end
+	def hamster_volume
+		quiet = "You've got quiet one"
+		loud = "You've got a loud one"
+
+		if @volume >= 6
+			p loud
+			p "====================="
+		else
+			p quiet
+			p "===================="
+		end
+	end
+
+	def fur
+		"#{@color} is a pretty color!"
+
+	end
+
+end
+
 puts "What's your Hamster's name?"
 name = gets.chomp
 puts "On a scale of one to 10 how lound is your hamster?"
 volume = gets.chomp.to_i
-
 puts "What color is #{name}'s fur"
 fur = gets.chomp
 puts "How old is #{name}"
-age = gets.chomp
+age = gets.chomp.to_i
 
-if age.length == 0
-	age = nil
-else 
-	puts age.to_i
-end
+new_hamster = Hamster.new(name, fur, volume, age)
+p new_hamster.hamster
 
-p "============================="
-puts "Hello #{name}"
-p "============================="
-
-if volume >= 6
-	puts "You've got loud one"
-else
-	puts "You've got a quiet one"
-end
-
-puts "#{fur} is a pretty color!"
-
-puts "your pet is #{age} years old?"
