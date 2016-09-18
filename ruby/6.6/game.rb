6e7e99510638e05934cde0a42cbd7e5b12e2ccaa
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-17 18:44:11 -0700

    game update

--- ruby/6.6/game.rb
+++ ruby/6.6/game.rb
@@ -1,77 +1,69 @@
 class Guessing
 	attr_reader :guess_count
-	attr_reader :game_on
+
 
 	def initialize(answer)
 		@right_answer = answer
-		@game_on = true
 		@guess_count = answer.length
+		@guesser_word = Array.new(@guess_count, "-")
 		p "Lets play!"
 	end
 
-	def your_word
-		p "-" * @guess_count
+	def your_word(guess)
+		@right_answer.each_index {|index|
+			if @right_answer[index] == guess
+			@guesser_word[index] = guess
+			@guesser_word
+		end
+		}
+		@guesser_word.join
 	end
 
 	def guess_check(guess)
-		@guess_count -=1
-		if @right_answer.include?(guess)
-			@game_on
+		check = []
+		guess.each_index { |index| 
+			if @right_answer[index] == guess[index]
+				check <<  guess[index]
+			end
+		}
+		
+		if check.include?(@right_answer.join)
+		 	"WINNER!""SORRY YOU LOSE!"
 		else
-			return false
+			"SORRY YOU LOSE!"
 		end
 	end
-
-	def final_check(final_answer)
-			p "Your Answer"
-			final_answer.join
-			p "Right Answer"
-			right_answer.join
-	end
 end
 
-
-final =[]
-
+final = []
 p "Player one input word"
-answer = gets.chomp.split
+answer = gets.chomp.chars
 
 game = Guessing.new(answer)
-game.your_word
+# game.your_word
+p answer
 
-p "Player two make a guess."
+while final.length != answer.length
+p "Player two make a guess"
 guess = gets.chomp
-
-
-case 
-when game.guess_check(guess) == true
-	p "Nice guess"
-	final << guess
-	p "Player two make a guess."
-	guess = gets.chomp
-when game.guess_check(guess) == false
-	p "Bad guess"
-	final << guess
-	p "Player two make a guess."
-	guess = gets.chomp
-when final.length == answer.length
-	game.final_check(final)
+p game.your_word(guess)
+# p answer
+final << guess
+break if final.length == answer.length	
+# # final << guess
+# end
+break if game.your_word(guess) == answer.join
+p final
 end
 
+ p game.guess_check(final)
 
 
 
 
 
-# while !
-# game.game_on
-# 	p "Player two make a guess"
-# 	guess = gets.chomp
-#   if game.guess_check(guess) == true
-#   	final << guess
-#   	p "Player two make a guess"
-# 	guess = gets.chomp
-#   end
-# end
 
-# puts "You won"
+
+
+
+
