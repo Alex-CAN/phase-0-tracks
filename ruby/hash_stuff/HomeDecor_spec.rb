cac6fdc5be7b0e68e64e084e9b7c97eb3ae52b8b
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-18 16:45:45 -0700

    rspecs

---
+++ ruby/hash_stuff/HomeDecor_spec.rb
@@ -0,0 +1,20 @@
+require_relative 'hashes'
+
+describe HomeDecor do
+	# let(:calculator) {Calculator.new}
+
+	# it "adds" do
+	# 	# expect(calculator.add(1,3)).to eq 4
+	# end
+	# it "subtracts two integers" do
+	#     # expect(calculator.subtract(7,1)).to eq 6
+	#   end
+
+	#   it "multiplies two integers" do
+	#     # expect(calculator.multiply(2,3)).to eq 6
+	#   end
+
+	#   it "divides two integers" do
+	#     # expect(calculator.divided(4,2)).to eq 2
+	#   end
+end
