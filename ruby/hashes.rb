d97540542eeb8276a8e446d13ad294eccaa8fd2e
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-18 00:07:57 -0700

    nesting data structures

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -4,26 +4,67 @@ def print aplicationhash
 def update key
 =end
 
+class Home_Decor
+	def initialize(name, address, email, phone, fave_shade_of_blue, wallpaper_prefernces, ombre)
+		@name = name
+		@address =address
+		@email =email
+		@phone = phone
+		@fave_shade_of_blue = fave_shade_of_blue
+		@wallpaper_prefernces =wallpaper_prefernces
+		@ombre = ombre
+		@applicants = {}
+	end
+end
+
 
 #create empty application hash
 application = Hash.new
 #Prompt user for application info and and it to the application hash
+=begin empty applicant hash
+loop: submit new applicant(y/n)
+until gets = n
+	get user input
+	initialize aplicant class
+=end
+
+puts "Would you like to apply to Home Decor?(y/n)?"
+x = gets.chomp
+
+until x == "n"
+
 p 'Welcome New Applicant! Please provide us with some basic info'
 p 'Name:'
-	application[:name] = gets.chomp
+	:name = gets.chomp
+	application[:name] = []
 p 'Address:'
-	application[:address] = gets.chomp
+	:address = gets.chomp
+	application[:name][:address]
 p 'Email:'
-	application[:email] = gets.chomp
+	application[:email][i] = gets.chomp
 p 'Phone number:'
-	application[:phone] = gets.chomp
+	application[:phone][i] = gets.chomp
 p 'What is your favorite shade of blue?'
-	application[:fave_shade_of_blue] = gets.chomp
+	application[:fave_shade_of_blue][i] = gets.chomp
 p "What is you're wallpaper prefernces?"
-	application[:wallpaper_prefernces] = gets.chomp
+	application[:wallpaper_prefernces][i] = gets.chomp
 p 'What do you think of ombre?'
-	application[:Ombre_is] = gets.chomp
+	application[:Ombre_is][i] = gets.chomp
 
+new_applicant = Home_Decor.new(:name, :address, :email, :phone, :fave_shade_of_blue, :wallpaper_prefernces, :Ombre_is)
+puts "Would you like to apply to Home Decor?(y/n)?"
+x = gets.chomp
+
+i =+ 1
+end
+
+
+
+=begin
+each_with_index
+push "apphash#{index}" =[#{apphash[index]}]  to applicanthash
+p aplicanthash
+=end
 # #printing hash keys and values
 # p application.keys
 
