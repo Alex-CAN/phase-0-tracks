579274d96189e0e35d245a7cb2bd269102832b22
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-19 11:59:03 -0700

    Java attempt

--- js/algos.js
+++ js/algos.js
@@ -3,10 +3,49 @@
 // compare array of lengths
 // print largest number
 
- var strings = ["abc", "blah", "ad"];
+//  var strings = ["abc", "blah", "ad"];
 
- strings.sort(function(a, b) {
- 	return a - b;
- });
+//  // strings.sort(function(a, b) {
+//  // 	return a - b;
+//  // });
 
- console.log(strings)
+//  // console.log(strings)
+
+// function sort(x) {for (var i= 0; i < x.length; i++) {
+// 	if (x[i].length < x[i + 1].length) {
+// 	console.log("lose");
+// }
+// else {
+// 	console.log(x[i]);
+// }
+// }
+// }
+ 
+// console.log(sort(strings));
+
+// ///so far i can acess the strings in the array by index. I set i to 0 and increment it to compare string[0] to the next string.
+// ///but i run into problems with the last string since there is no next index
+// ///i thought to solve this by starting with -1 and add up but for some reason it wont even access that -1
+
+// var dog_0 = {name: 'Bear', age: 13, isGoodDog: false};
+// var dog_1 = {name: 'Chai', age: 13, isGoodDog: true};
+var dog_house = this;
+
+function Dog(name, age, isGoodDog) {
+    console.log("NEW Doggy", this);
+    this.name = name;
+    this.age = age;
+    this.isGoodDog = isGoodDog;
+
+    // this.rev = function () {console.log("Vroom Vroom!"); };
+}
+
+var dog_0 = new Dog("Bear", 13, false);
+var dog_1 = new Dog("Chai", 13, true);
+
+console.log(dog_0)
+console.log(dog_1)
+
+
+
+
--- js/explore.js
+++ js/explore.js
@@ -2,16 +2,21 @@
 //for the length of the string we want to print each of the the indices in descending order
 
 
-// str = ("hello")
+str = ("hello")
 
 function reverse(x) {for (var i = x.length; i > -1; i -= 1) {
-	console.log(x[i]);
-}
-}
-
-if (1 == 9) {
+	if (1 == 9) {
 	console.log("lose");
 }
 else {
-console.log(reverse("hello"));
+	x[i] = x[i].length 
+	console.log(x[i]);
 }
+}
+}
+
+ 
+console.log(reverse(str));
+
+
+
