0f1ad87afcea7d839b6366528167cfe3a761af21
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-22 14:14:37 -0700

    commit for branch change

---
+++ ruby/week_5/5.2.rb
@@ -0,0 +1,9 @@
+y  =true
+n = false
+model_application = {name: "Tyra Banks", dob: "12/13/69", address: "1234 st", height:"6ft 3in", weight: "125lbs" }
+model_application[:hired] = y
+p model_application
+
+model_application[:name] + model_application[:dob]
+puts model_application[:name] + model_application[:dob]
+puts model_application.fetch(:name)
---
+++ ruby/week_5/method_detective.rb
@@ -0,0 +1,38 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+
+# p "iNvEsTiGaTiOn".swapcase
+# => “InVeStIgAtIoN”
+
+# "zom".insert(2, "o")
+# => “zoom”
+
+# "enhance".center(15)
+# => "    enhance    "
+
+# "Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
+
+# "the usual".insert(9, " suspects")
+#=> "the usual suspects"
+
+# " suspects".insert(0, "the usual")
+# => "the usual suspects"
+
+# "The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette"
+
+# p "The mystery of the missing first letter".delete[0]
+# => "he mystery of the missing first letter"
+
+# "Elementary,    my   dear        Watson!".squeeze(" ")
+# => "Elementary, my dear Watson!"
+
+# "z".ord
+# => 122 
+# it's the integer ordinal
+#"How many times does the letter 'a' appear in this string?".count "a"
+# => 4
