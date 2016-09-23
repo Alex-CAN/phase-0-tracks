f2f7c3ba856d017f4d49f6ec778b843f57f215c2
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-23 16:43:04 -0700

    add to hash mthod

--- ruby/week_5/5.2.rb
+++ ruby/week_5/5.2.rb
@@ -1,9 +1,27 @@
-y  =true
-n = false
-model_application = {name: "Tyra Banks", dob: "12/13/69", address: "1234 st", height:"6ft 3in", weight: "125lbs" }
-model_application[:hired] = y
-p model_application
+# y  =true
+# n = false
+# model_application = {name: "Tyra Banks", dob: "12/13/69", address: "1234 st", height:"6ft 3in", weight: "125lbs" }
+# model_application[:hired] = y
+# p model_application
 
-model_application[:name] + model_application[:dob]
-puts model_application[:name] + model_application[:dob]
-puts model_application.fetch(:name)
+# model_application[:name] + model_application[:dob]
+# puts model_application[:name] + model_application[:dob]
+# puts model_application.fetch(:name)
+
+
+model_application = {
+applicants: {
+		"" => { dob: "", address: "", phone: "", height:"", weight: "" }
+	}
+}
+
+def new_applicant(name, dob, address, phone, height, weight)
+	model_application[:applicants][name][:dob] = 
+	model_application[:applicants][name][:address] =
+	model_application[:applicants][name][:phone] =
+	model_application[:applicants][name][:height] = 
+	model_application[:applicants][name][:weight] =
+
+
+
+end
