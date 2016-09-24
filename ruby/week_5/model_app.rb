f5b53c951fc7dbaee71402147eb57ab84f375c13
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-23 22:29:19 -0700

    iterating through hashes

--- ruby/week_5/model_app.rb
+++ ruby/week_5/model_app.rb
@@ -2,42 +2,66 @@ class FashionShow
 
 	def initialize
 		@catalog = {}
-		@model = name
-		@designer = designer
-		@outfit = wearing
-		@rank = rank
-
+		@model_lineup =model_lineup = {
+			"Gisele Bundchen" => {designer: "Versace", outfit: "Little Black Dress", rank: ""},
+			"Naomi Campbel" => {designer: "Vouge", outfit: "Silver Gown", rank: ""},
+			"Kate Moss" => {designer: "Jamie Hince", outfit: "Blue Frock", rank: ""},
+			"Adrian Lima" => {designer: "Christian Lacroix", outfit: "Red n Black pin stripe", rank: ""},
+			"Cindy Crawford" => {designer: "Ann Klenn", outfit: "Orange Sport Frill", rank: ""}
+		}
+		@walking = true
 	end
 
+	def call
+		p catwalk
+	end
+	# method introduces <model> and <designer> to runway
 	def enter_runway
-		"Now entering the walk way is #{model} wearing #{designer}"
+		if @walking
+			@model_lineup.each {|model, info|
+		puts "Now entering the walk way is #{model} wearing #{info.first}"}
+		end
+	
 	end
-
+	# method {enter_runway}. Presents <model>s <outfit>. logs <model><designer><outfit><rank> to catalog. {exit_runway}. 
+	def catwalk
+		@walking
+		puts enter_runway
+		# @model_lineup.each {|model, info| p "#{key} , #{info[0]}, info[1]"}
+	end
+	# method exits <model> from runway
 	def exit_runway
 		"#{model} exits runway."
 	end
 
 	def display
 	end
-
+	#iterate through array to sort lowest to highest rank. return last three aka highest rank.
 	def top_three
 	end
 
 end
 
-=begin upon intialization of Fashion show class user is prompted to enter fashion show.
-if yes model enters.
-user is prompted for ranking
-model exits
-loop through hash of 5 models. 
-prompt user if theyd like to see a display of all models or top three
-return display of all models or top three
+
+new_show = FashionShow.new
+p new_show.call
+
+=begin (1)upon intialization of Fashion show class user is prompted to enter fashion show.
+(2)if yes model enters.
+(3)user is prompted for ranking
+(4)model exits
+(5)loop through hash of 5 models. 
+(6)prompt user if theyd like to see a display of all models or top three
+(7)return display of all models or top three
 =end
 
-model_lineup = {
-	"Gisele Bundchen" => {designer: "", outfit: "", rank: ""},
-	"Naomi Campbel" => {designer: "", outfit: "", rank: ""}
-	"Kate Moss" => {designer: "", outfit: "", rank: ""}
-	"Adrian Lima" => {designer: "", outfit: "", rank: ""}
-	"Cindy Crawford" => {designer: "", outfit: "", rank: ""}
-}
+
+
+# (1)
+# puts "Welcome to My Hashion Show (All puns intended)! Would you like to enter?(y/)"
+# enter = gets.chomp.downcase
+# #(2)
+# 	if enter == "y"
+# 		walking = true
+
+# 	end
