9a5589ecb7a09cb2bd9cb9a001fc1e5bd95b7911
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-23 19:51:46 -0700

    adding to hashes and displaying data

--- ruby/week_5/5.2.rb
+++ ruby/week_5/5.2.rb
@@ -9,19 +9,45 @@
 # puts model_application.fetch(:name)
 
 
-model_application = {
-applicants: {
-		"" => { dob: "", address: "", phone: "", height:"", weight: "" }
-	}
-}
-
-def new_applicant(name, dob, address, phone, height, weight)
-	model_application[:applicants][name][:dob] = 
-	model_application[:applicants][name][:address] =
-	model_application[:applicants][name][:phone] =
-	model_application[:applicants][name][:height] = 
-	model_application[:applicants][name][:weight] =
+# model_application = {
+# applicants: {
+# 		"" => { dob: "", address: "", phone: "", height:"", weight: "" }
+# 	}
+# }
 
 
+def new_applicant(hash, model,dob, address, phone, height, weight,hired)	
+	hash[:applicants] =Hash.new
+	hash[:applicants][model] =Hash.new
+	hash[:applicants][model][:dob] = dob
+	hash[:applicants][model][:address] = address
+	hash[:applicants][model][:phone] = phone
+	hash[:applicants][model][:height] = height
+	hash[:applicants][model][:weight] =weight
+	hash[:applicants][model][:hired] = hired
+	hash
+end
+
+#display applicant
+
+def display(application)
+	application.each do |key, value|
+	puts "========APPLICANT: application[:applicants][#{application[:applicants]}]=========="
+	puts "#{value}"
+	end
 
 end
+#update applicant info hired 
+#delete applicant
+model_application = Hash.new
+
+model = "Tyra Banks"
+dob = "12/13/69" 
+address = "1234 st"
+phone = "555-1243"
+height = "6ft 3in"
+weight = "125lbs" 
+hired = "pending"
+
+new_applicant(model_application, model, dob, address,phone, height,weight,hired)
+p display(model_application)
