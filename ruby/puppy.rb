0da5a7abc37bed1abba705c1d9a2aaaa968b8d9b
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-26 13:34:34 -0700

    inheriting methods from modules.

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -1,3 +1,14 @@
+# establishing common characteristics and applying them to a puppy module to be included in the indiviual species clases
+module Puppy
+	def bark
+		 "BARK"
+	end
+	def run
+		 "I'm running!"
+	end
+end
+
+
 # <<<<<<<<<<<<<<<<<<<<<<< SPECIES NAME >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # pomeranian
 
@@ -17,16 +28,23 @@
 # simultaneously be the most annoying and adorable thing in the room
 
 class Palmeranian
+	def initialize
+		puts "bark bark bark..."
+	end
+	include Puppy
 end
 
 chai = Palmeranian.new
 cavi = Palmeranian.new
 coco = Palmeranian.new
 
-p coco.class
-p cavi == chai
-p chai.instance_of?(Array)
-p chai.instance_of?(Palmeranian)
+puts coco.class
+puts cavi == chai
+puts chai.instance_of?(Array)
+puts chai.instance_of?(Palmeranian)
+
+puts cavi.run
+puts coco.bark
 
 puts
 puts "==================================================================================================================="
@@ -50,8 +68,15 @@ puts
 
 
 class Husky
+	include Puppy
+	def play(toy)
+		"Im playing with #{toy}"
+	end
 end
 
 shea = Husky.new
 
-p shea.class
+puts shea.class
+puts shea.bark
+puts shea.run
+puts shea.play("ball")
