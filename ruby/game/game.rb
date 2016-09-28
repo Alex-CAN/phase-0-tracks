1b1a7986b8fdf0e81b40f958fb69b2114d150083
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-27 20:48:05 -0700

    spec test on method:decrypt

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -6,6 +6,21 @@ class Game
 			encrypted_word << "-"
 		end
 		return encrypted_word.join
-		
+	end
+
+	def decrypt(secret_word, guess_letter)
+		word = secret_word.chars
+		word.each do |letter|
+			if letter != guess_letter
+				letter.gsub!(/["#{letter}"]/, "-")
+			end
+		end
+		return word.join
 	end
 end
+
+
+# new_game =  Game.new
+# p new_game.encrypt("word")
+
+# p new_game.decrypt("word", "r")
--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,8 +1,20 @@
 require_relative 'game'
 
 describe Game do
-	let(:new_game) {Game.new}
+	let(:new_game) { Game.new }
+
 	it "takes a password string and returns a a string of dashes" do
-		expect (new_game.encrypt("word")).to eq "----"
+		expect(new_game.encrypt("word")).to eq "----"
 	end
+
+	it "takes a guess letter and string of dashes. returns string with dashes replaced by letter respectively. " do
+		expect(new_game.decrypt("word", "r")).to eq "--r-"
+	end
+
 end
+
+
+#player one inputs secretword to be encrypted
+#player two inputs guess letter
+#method takes letter and compares it with each of the letters in the secretword
+#returns string with dashes and filled in dashes.
