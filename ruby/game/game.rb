966683eb626d82303fa1e3178635f1e60f5e156d
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-28 14:15:56 -0700

    game:player 2 user interface

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,5 +1,11 @@
 class Game
-	def encrypt(secret_word)
+	attr_accessor :secret_word
+
+	def initialize(secret_word)
+		@secret_word = secret_word
+	end
+	#PLAYER 1
+	def encrypt
 		word = secret_word.chars
 		encrypted_word = []
 		word.each do|let| 
@@ -8,19 +14,59 @@ class Game
 		return encrypted_word.join
 	end
 
-	def decrypt(secret_word, guess_letter)
-		word = secret_word.chars
-		word.each do |letter|
-			if letter != guess_letter
-				letter.gsub!(/["#{letter}"]/, "-")
-			end
+
+
+	#PLAYER 2
+	def dash(let, guess_let)
+		if let != guess_let
+			"-"
+		else
+			return let
 		end
-		return word.join
 	end
+
+	def decrypt(guess_letter)
+		arr = []
+		word = secret_word.chars
+		word.each_index do |ind|
+			arr << dash(word[ind],guess_letter)
+		end
+		return arr.join
+	end
+
+	def guess_check(guess)
+		if guess == secret_word
+			"WINNER"
+		else
+			"LOSER"
+		end
+
+	end
+
 end
 
 
-# new_game =  Game.new
-# p new_game.encrypt("word")
+# new_game =  Game.new("word")
+# # p new_game.encrypt("word")
 
-# p new_game.decrypt("word", "r")
+# # p new_game.decrypt("word", "r")
+# p new_game.decrypt("r")
+
+puts 'player 1'
+word = gets.chomp
+new_game = Game.new(word)
+
+# word.length times get player 2's guess letter and puts decryption and psuh to a guess array
+# if guess array.include? gets guess give player another guess
+#join decrypted array and compare to word
+
+
+guesses = word.length
+final = []
+while guesses != 0 
+	puts 'player 2'
+	guess_letter = gets.chomp
+	final << guess_letter
+	new_game.decrypt(guess_letter)
+	guesses -= 1
+end
