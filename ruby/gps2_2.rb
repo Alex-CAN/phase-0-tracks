f40187d1eedf57b7a5d80c9c8bd499bc99fd54f8
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-30 17:50:10 -0700

    fix merge issues and comment out test driver code

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-#GPS 2.2
-=======
 #GPS 2.2# Method to create a list
-=======
-#GPS 2.2
 
 # Method to create a list
->>>>>>> game
 # input: string of items separated by spaces, optional default quantity
 # steps: 
   # Create an empty hash list
@@ -16,29 +9,21 @@
   # Iterate through the array list, and add each item-quantity pair into hash list
   # print the list to the console
 # output: Hash list
-<<<<<<< HEAD
 
-def create_list(str, default_quantity=1)
-  list = {}
-  item_array = str.split
-  item_array.each do |item|
-    list[item] = default_quantity
-  end
-  print_list(list)
-  return list
-end
-=======
+
+
+
 class GroceryList
+
 	def create_list(str, default_quantity=1)
 	  list = {}
 	  item_array = str.split
 	  item_array.each do |item|
 	    list[item] = default_quantity
-	  end
+	   end
 	  print_list(list)
 	  return list
 	end
->>>>>>> game
 
 # Method to add an item to a list
 # input: item name, list, quantity
@@ -79,21 +64,17 @@ end
 
 def print_list(list)
   puts "Name \t\t Quantity"
-  list.each do |key, value|
-    puts "#{key} \t\t #{value}"
+    list.each do |key, value|
+  puts "#{key} \t\t #{value}"
   end
 end
 
-<<<<<<< HEAD
-test_string = "Apple Grape Cabbage"
-=======
 end
 
 
 
 # test code
 =begin test_string = "Apple Grape Cabbage"
->>>>>>> game
 list_1 = create_list(test_string)
 
 add_item("Tea", list_1, 3)
@@ -105,11 +86,6 @@ p list_1
 update_quantity("Apple", list_1, 5)
 p list_1
 print_list(list_1)
-<<<<<<< HEAD
->>>>>>> 6.3
-=======
 =end
 
-test_string = "a b c"
-list_1 = create_list(test_string)
->>>>>>> game
+
