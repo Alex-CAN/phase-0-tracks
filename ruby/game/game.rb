e2ab954f1c97baa1192d02814eb9336d5a061868
Alex C. Nelon <caminelson.cn@gmail.com>
2016-09-30 17:23:45 -0700

    fix merge issues, add psuedo, comment out driver

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -9,12 +9,6 @@ class Game
 	end
 
 	def encrypt
-		# word = secret_word.chars
-		# encrypted_word = []
-		# word.each do|let| 
-		# 	encrypted_word << "-"
-		# end
-		# return encrypted_word.join
 		@guesser_word.join
 	end
 	
@@ -33,31 +27,6 @@ class Game
 
 	end
 
-	def dash(let, guess_let)
-		if let != guess_let
-			"-"
-		else
-			return let
-		end
-	end
-
-	def decrypt(guess_letter)
-		arr = []
-		word = secret_word.chars
-		word.each_index do |ind|
-			arr << dash(word[ind],guess_letter)
-		end
-		return arr.join
-	end
-
-	def guess_check(guess)
-		if guess == secret_word
-			"WINNER"
-		else
-			"LOSER"
-		end
-
-	end
 
 	def guess_check(final)
 		check_array = []
@@ -77,35 +46,20 @@ class Game
 	end
 end
 
-
+=begin TEST DRIVER
 # new_game =  Game.new("word")
 # # p new_game.encrypt("word")
 
 # # p new_game.decrypt("word", "r")
 # p new_game.decrypt("r")
+=end
 
-<<<<<<< HEAD
-# puts 'player 1'
-# word = gets.chomp
-# new_game = Game.new(word)
 
 # # word.length times get player 2's guess letter and puts decryption and psuh to a guess array
 # # if guess array.include? gets guess give player another guess
 # #join decrypted array and compare to word
+#require i/o method to use a noehco on console to hide player one's user input
 
-
-# guesses = word.length
-# final = []
-# while guesses != 0 
-# 	puts 'player 2'
-# 	guess_letter = gets.chomp
-# 	final << guess_letter
-# 	new_game.decrypt(guess_letter)
-# 	guesses -= 1
-# end
-
-=======
->>>>>>> game_1
 
 # require 'io/console'
 
@@ -116,6 +70,8 @@ end
 # # word.length times get player 2's guess letter and puts decryption and psuh to a guess array
 # # if guess array.include? gets guess give player another guess
 # #join decrypted array and compare to word
+
+
 # puts 
 # puts
 # puts
@@ -123,6 +79,8 @@ end
 # puts
 # puts
 # puts
+
+
 # puts "GUESSING GAME"
 # puts " Your Word: #{new_game.encrypt}"
 # guesses = (word.length)
@@ -141,8 +99,5 @@ end
 # end
 
 # puts word
-<<<<<<< HEAD
 # puts new_game.guess_check(final)
-=======
 # puts new_game.guess_check(final)
->>>>>>> game_1
