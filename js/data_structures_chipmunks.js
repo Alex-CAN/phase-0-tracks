0b7e2a0f00dabce947d1dea4b65697f42167a2e9
Alex Nelson <caminelson.cn@gmail.com>
2016-10-06 19:33:22 -0700

    function Car

--- js/data_structures_chipmunks.js
+++ js/data_structures_chipmunks.js
@@ -14,3 +14,18 @@ for (var i = 0 ; i < colors.length; i++) {
 }
 
 console.log(barn);
+
+
+function Car(year,make,color,awd) {
+	console.log("Our new car:", this);
+	this.year = year;
+	this.make = make;
+	this.color = color;
+	this.awd = awd;
+	this.rev = function() {console.log("Vrooooom!");};
+	console.log("car initialization complete");
+}
+
+var newCar = new Car(1989,"Ford","red",true);
+console.log(newCar);
+newCar.rev();
