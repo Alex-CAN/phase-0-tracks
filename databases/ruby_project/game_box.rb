d37149094b643a279323fbd06da1bfc58fe3c9db
Alex Nelson <caminelson.cn@gmail.com>
2016-10-17 13:40:33 -0700

    create table

--- databases/ruby_project/game_box.rb
+++ databases/ruby_project/game_box.rb
@@ -2,17 +2,17 @@ require 'sqlite3'
 require 'faker'
 require 'babbage'
 
-db = SQLite3::Database.new("game_box.db")
+db =SQLite3::Database.new("game_box.db")
 
-create_table_cmd = <<-SQL
-  CREATE TABLE IF NOT EXISTS gamers(
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255),
-    high_score INT
-  )
-SQL
+# create_table_cmd = <<-SQL
+#    CREATE TABLE IF NOT EXISTS gamers(
+#     id INTEGER PRIMARY KEY,
+#     name VARCHAR(255),
+#     high_score INT
+#   )
+#  SQL
 
-db.execute(create_table_cmd)
+# db.execute(create_table_cmd)
 
 # scores = [23,2,17,33,49]
 
